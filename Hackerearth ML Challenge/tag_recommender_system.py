033c82f36c3ef44373960e158500e79393251ee3
shaswat <shaswatlenka10@outlook.com>
2018-11-09 01:10:32 +0530

    add bag of words model

--- Hackerearth ML Challenge/tag_recommender_system.py
+++ Hackerearth ML Challenge/tag_recommender_system.py
@@ -2,9 +2,15 @@ import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import matplotlib.pyplot as plt
 import re
+import string
+import os
 import nltk #NLP library
 nltk.download('stopwords')
 from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer #to perform stemming
+#from bs4 import BeautifulSoup #uncomment if you like to use BeautifulSoup instead
+from sklearn.feature_extraction.text import TfidfVectorizer
+
 
 #define chunk size for big dataset
 chunk_size = 50000
@@ -17,8 +23,23 @@ for chunk in dataset:
    break
 
 
-article = re.sub('[^a-zA-Z]', ' ', df['article'][0])
-article = article.lower()
-article = article.split()
-article = [word for word in article if not word in set(stopwords.words('english')) and len(word) != 1] 
+#stemmer object
+#NOTE: I think stemming in this case is not important as we only deal with the technical terms so the words/tags
+#themselves are the root words
+ps = PorterStemmer()
 
+#Another way of doing it but I found it not accurate enough
+#soup = BeautifulSoup(df['article'][0], 'html.parser')
+#article = soup.get_text()
+
+#creating the corpus for all the articles
+corpus = []
+
+for i in range(0,1000):
+    article = re.sub('[^a-zA-Z]', ' ', df['article'][i])
+    article = article.lower()
+    article = article.split()
+    article = [ps.stem(word) for word in article if not word in set(stopwords.words('english')) and len(word) != 1] 
+    article = ' '.join(article)
+    corpus.append(article)
+#Create the bag of words model
